23adc603b393fe491d42d4e1e22f37740b3bd57d
hardefarogonondo <hardefarogonondo@gmail.com>
2024-03-09 01:53:26 +0700

    Finished backend API updated

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,19 +1,20 @@
 # Import Libraries
 from config.config import SummarizeRequest
 from fastapi import FastAPI, HTTPException
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 
 # Initialization
-tokenizer = T5Tokenizer.from_pretrained('../../models/t5_tokenizer')
-model = T5ForConditionalGeneration.from_pretrained('../../models/t5_model')
+model_dir = '../../models/pegasus_model'
+tokenizer = PegasusTokenizer.from_pretrained(model_dir)
+model = PegasusForConditionalGeneration.from_pretrained(model_dir)
 app = FastAPI()
 
 
 @app.post("/summarize")
 def summarize(request: SummarizeRequest):
     try:
-        input_ids = tokenizer.encode(request.text, return_tensors='pt', add_special_tokens=True)
-        summary_ids = model.generate(input_ids, num_beams=5, max_length=200, early_stopping=True)
+        input_ids = tokenizer.encode(request.text, return_tensors='pt', add_special_tokens=True, truncation=True, max_length=1024)
+        summary_ids = model.generate(input_ids, max_length=200, min_length=30, length_penalty=1.5, num_beams=6, early_stopping=True)
         summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
         return {"summary": summary}
     except Exception as error_msg:
